f7b9b0e763fd91fab76d6ce86b5ecf927a9d8977
isaac_fletcher <ifletch@clemson.edu>
2023-11-11 19:19:35 -0500

    Added Dialogue Scene + Assets.

--- Assets/Scripts/Interactions/Button.cs
+++ Assets/Scripts/Interactions/Button.cs
@@ -1,37 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Button : MonoBehaviour
+namespace Interactions
 {
-    public GameObject door1;
-    public GameObject door2;
-
-    private bool door1StayOpen;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Button : MonoBehaviour
     {
-        door1StayOpen = false;
-    }
+        public GameObject door1;
+        public GameObject door2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (door1StayOpen)
-            door1.SetActive(false);
-    }
+        private bool door1StayOpen;
 
+<<<<<<< HEAD:Assets/Scripts/Interactions/Button.cs
     private void OnTriggerEnter2D(Collider2D c)
     {
         if (c.gameObject.CompareTag("Player") || c.gameObject.CompareTag("Block"))
+=======
+        // Start is called before the first frame update
+        void Start()
+>>>>>>> ebd5fe2 (Added Dialogue basics):Assets/Interactions/Button.cs
         {
-            door2.SetActive(false);
-            door1StayOpen = true;
+            door1StayOpen = false;
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            door2.SetActive(true);
+            if (door1StayOpen)
+                door1.SetActive(false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D c)
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+                door2.SetActive(false);
+                door1StayOpen = true;
+            }
+            else
+            {
+                door2.SetActive(true);
+            }
         }
     }
 }
