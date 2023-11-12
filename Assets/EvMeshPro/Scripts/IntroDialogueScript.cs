73e2a16bd435d6d6a480b8bc0f0ca9e86851a285
superbode <145613869+superbode@users.noreply.github.com>
2023-11-11 19:55:15 -0500

    Resolved merge conflicts + re-added intro sequence

--- Assets/EvMeshPro/Scripts/IntroDialogueScript.cs
+++ Assets/EvMeshPro/Scripts/IntroDialogueScript.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueExample : MonoBehaviour
+public class IntroDialogueScript : MonoBehaviour
 {
 	// The note image object
 	public Image note;
