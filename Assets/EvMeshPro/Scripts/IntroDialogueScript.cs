268e0eac0c217c64af2b47d3159f5f9de783c10e
superbode <145613869+superbode@users.noreply.github.com>
2023-11-11 19:10:22 -0500

    Addition of SFX and music

--- Assets/EvMeshPro/Scripts/IntroDialogueScript.cs
+++ Assets/EvMeshPro/Scripts/IntroDialogueScript.cs
@@ -8,6 +8,7 @@ public class DialogueExample : MonoBehaviour
 {
 	// The note image object
 	public Image note;
+	public AudioSource noteReveal;
 
 	private void Start() {
 		// Disables rendering the note object
@@ -19,16 +20,18 @@ public class DialogueExample : MonoBehaviour
 	}
 
 	IEnumerator introDialogueSequence(){
-		
+		// Wait 2 seconds for smoother transition
+		yield return new WaitForSecondsRealtime(2);
+
 		// Elara
-		DialogueController.instance.NewDialogueInstance("Aiden… I can barely see!","character_elara");
+		DialogueController.instance.NewDialogueInstance("Aiden. . . I can barely see!","character_elara");
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("Shh! These sewers are echoey, we gotta make sure no one’s creepin’ on us, Elara.", "character_aiden");
 
 		// Elara
 		DialogueController.instance.NewDialogueInstance("Ugh… it’s not like we’re going to find radioactive rat mutants that understand English.", "character_elara");
-		DialogueController.instance.NewDialogueInstance("...", "character_elara");
+		DialogueController.instance.NewDialogueInstance(". . .", "character_elara");
 		DialogueController.instance.NewDialogueInstance("Are you even sure this is the right way?", "character_elara");
 
 		// Aiden
@@ -44,26 +47,41 @@ public class DialogueExample : MonoBehaviour
 		DialogueController.instance.NewDialogueInstance("You said that pretty loud yourself.", "character_elara");
 
 		// Aiden
-		DialogueController.instance.NewDialogueInstance("Urgh…", "character_aiden");
+		DialogueController.instance.NewDialogueInstance("Urgh. . .", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("I guess it doesn’t matter how loud we are in here.", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("To be honest, I’m ready to see what Mom and Dad left for us in these sewers under Lumina.", "character_aiden");
 
 		// Elara
-		DialogueController.instance.NewDialogueInstance("Yeah…", "character_elara");
+		DialogueController.instance.NewDialogueInstance("Yeah. . .", "character_elara");
 		DialogueController.instance.NewDialogueInstance("We’ve had that note for years now, but it was only yesterday we figured out that the sewer layout matched the weird lines scribbled on the note pretty well.", "character_elara");
 		DialogueController.instance.NewDialogueInstance("I’m just hoping you know how to read a map.", "character_elara");
 
-		// NOTE POPS ONTO SCREEN (AFTER 60 SECONDS IN REAL TIME)
-		yield return new WaitForSecondsRealtime(60);
+		// NOTE POPS ONTO SCREEN (AFTER 65 SECONDS IN REAL TIME)
+		yield return new WaitForSecondsRealtime(65);
+
+		// Plays note reveal SFX
+		noteReveal.Play();
+
+		// Image of note is displayed once enabled
 		note.enabled = true;
 
+
 		// Aiden
-		DialogueController.instance.NewDialogueInstance("Right…", "character_aiden");
+		DialogueController.instance.NewDialogueInstance("Right. . .", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("How many years has it been now? A little under a decade or somethin’, right?", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("It’s surprising Lumina is looking the way it is now according to those pictures Auntie and Uncle showed us, but after what happened I guess it’s to be expected.", "character_aiden");
 
+		// After some time, disable note image
+		yield return new WaitForSecondsRealtime(7);
+
+		// Plays note reveal SFX
+		noteReveal.Play();
+
+		// Image of note undisplayed
+		note.enabled = false;
+
 		// Elara
-		DialogueController.instance.NewDialogueInstance("9 years. It’s been about 9 years and a few months…", "character_elara");
+		DialogueController.instance.NewDialogueInstance("9 years. It’s been about 9 years and a few months. . .", "character_elara");
 		DialogueController.instance.NewDialogueInstance("I only keep track of the anniversary, though. I don’t want to remind myself about what’s happened that often.", "character_elara");
 
 		// Aiden
@@ -71,7 +89,7 @@ public class DialogueExample : MonoBehaviour
 		DialogueController.instance.NewDialogueInstance("As far as we know, they disappeared ‘cause of the pressure put on them by those corporation guys Uncle won’t stop talking about.", "character_elara");
 
 		// Elara
-		DialogueController.instance.NewDialogueInstance("I didn’t say that! I’m just saying I miss them, Aiden…", "character_elara");
+		DialogueController.instance.NewDialogueInstance("I didn’t say that! I’m just saying I miss them, Aiden. . .", "character_elara");
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("I miss them, too, Elara. Here’s to hoping we find them.", "character_aiden");
