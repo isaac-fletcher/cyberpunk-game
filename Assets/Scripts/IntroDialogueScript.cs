33d7f7f4dc9abbdcdfc2dddfa373eb7a56512b21
superbode <145613869+superbode@users.noreply.github.com>
2023-11-11 21:18:51 -0500

    Moved file structure, added lighter sfx to intro sequence, added controller layout animation to intro sequence, re-timed note display in intro sequence, toned down some sfx and music in intro sequence

--- Assets/Scripts/IntroDialogueScript.cs
+++ Assets/Scripts/IntroDialogueScript.cs
@@ -8,15 +8,18 @@ public class IntroDialogueScript : MonoBehaviour
 {
 	// The note image object
 	public Image note;
+	public Image controlLayout;
 	public AudioSource noteReveal;
+	public AudioSource lighterFlicking;
 
 	private void Start() {
-		// Disables rendering the note object
+
+		// Disables rendering the note and controller layout
 		note.enabled = false;
+		controlLayout.enabled = false;
 
 		// Runs the Introduction Dialogue Sequence
 		StartCoroutine(introDialogueSequence());
-		
 	}
 
 	IEnumerator introDialogueSequence(){
@@ -25,20 +28,30 @@ public class IntroDialogueScript : MonoBehaviour
 
 		// Elara
 		DialogueController.instance.NewDialogueInstance("Aiden. . . I can barely see!","character_elara");
-
+		
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("Shh! These sewers are echoey, we gotta make sure no one’s creepin’ on us, Elara.", "character_aiden");
-
+		
 		// Elara
-		DialogueController.instance.NewDialogueInstance("Ugh… it’s not like we’re going to find radioactive rat mutants that understand English.", "character_elara");
+		DialogueController.instance.NewDialogueInstance("Ugh. . . it’s not like we’re going to find radioactive rat mutants that understand English.", "character_elara");
 		DialogueController.instance.NewDialogueInstance(". . .", "character_elara");
 		DialogueController.instance.NewDialogueInstance("Are you even sure this is the right way?", "character_elara");
+
+		// NOTE POPS ONTO SCREEN (AFTER 22 SECONDS IN REAL TIME)
+		yield return new WaitForSecondsRealtime(22);
+		noteReveal.Play(); 	// Plays note reveal SFX
+		note.enabled = true; // Image of note is displayed once enabled
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("The layout is matchin’ the note perfectly.", "character_aiden");
 
 		//Elara
 		DialogueController.instance.NewDialogueInstance("How can you even tell? It’s so dark!", "character_elara");
+
+		// After some time, disable note image
+		yield return new WaitForSecondsRealtime(7);
+		noteReveal.Play(); // Plays note reveal SFX
+		note.enabled = false; // Image of note undisplayed
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("What’d I say about being quiet!?", "character_aiden");
@@ -56,29 +69,11 @@ public class IntroDialogueScript : MonoBehaviour
 		DialogueController.instance.NewDialogueInstance("We’ve had that note for years now, but it was only yesterday we figured out that the sewer layout matched the weird lines scribbled on the note pretty well.", "character_elara");
 		DialogueController.instance.NewDialogueInstance("I’m just hoping you know how to read a map.", "character_elara");
 
-		// NOTE POPS ONTO SCREEN (AFTER 65 SECONDS IN REAL TIME)
-		yield return new WaitForSecondsRealtime(65);
-
-		// Plays note reveal SFX
-		noteReveal.Play();
-
-		// Image of note is displayed once enabled
-		note.enabled = true;
-
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("Right. . .", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("How many years has it been now? A little under a decade or somethin’, right?", "character_aiden");
 		DialogueController.instance.NewDialogueInstance("It’s surprising Lumina is looking the way it is now according to those pictures Auntie and Uncle showed us, but after what happened I guess it’s to be expected.", "character_aiden");
-
-		// After some time, disable note image
-		yield return new WaitForSecondsRealtime(7);
-
-		// Plays note reveal SFX
-		noteReveal.Play();
-
-		// Image of note undisplayed
-		note.enabled = false;
 
 		// Elara
 		DialogueController.instance.NewDialogueInstance("9 years. It’s been about 9 years and a few months. . .", "character_elara");
@@ -86,7 +81,7 @@ public class IntroDialogueScript : MonoBehaviour
 
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("Elara, you say that like our parents died, but we have no way of knowing that.", "character_aiden");
-		DialogueController.instance.NewDialogueInstance("As far as we know, they disappeared ‘cause of the pressure put on them by those corporation guys Uncle won’t stop talking about.", "character_elara");
+		DialogueController.instance.NewDialogueInstance("As far as we know, they disappeared ‘cause of the pressure put on them by those corporation guys Uncle won’t stop talking about.", "character_aiden");
 
 		// Elara
 		DialogueController.instance.NewDialogueInstance("I didn’t say that! I’m just saying I miss them, Aiden. . .", "character_elara");
@@ -102,11 +97,16 @@ public class IntroDialogueScript : MonoBehaviour
 		// Aiden
 		DialogueController.instance.NewDialogueInstance("Right, yeah, here ya go. Totally forgot I brought it.", "character_aiden");
 
-		//
-		// FLICKING LIGHTER SFX PLAY
-		// KEYBOARD AND GAMEPAD LAYOUTS DISPLAY
-		// FADE INTO TUTORIAL LEVEL
-		//
-		
+		// After dialogue, play lighter flicking sfx and reveal controller layout
+		yield return new WaitForSecondsRealtime(140);
+		lighterFlicking.Play(); // Play lighter flicking sfx
+
+		// Wait slightly longer before revealing controller layout
+		yield return new WaitForSecondsRealtime(5);
+		controlLayout.enabled = true; // Animation of controller layout displayed
+
+		// Wait for 6 seconds before undisplaying controller layout
+		yield return new WaitForSecondsRealtime(7);
+		controlLayout.enabled = false; // Animation of controller layout undisplayed
 	}
 }
