59b6d86051a8964cb9e2a9c77c674ee9f7e1c908
superbode <145613869+superbode@users.noreply.github.com>
2023-12-01 15:39:20 -0500

    Added panning that slows near top of background screen to TowerCutscene

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,22 +5,78 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
 
-    public bool upDownMovement;
+    public bool upMovement;
+
+    public bool shouldStop;
+
+    public float stopAtPosition;
 
     public float movementSpeed;
+
+    private Vector3 startPos;
+
+    private float stopPoint;
+
+    private float newStopPoint;
+    
+    private int slowDownIterator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        slowDownIterator = 2;
+
+        if (!upMovement){
+            stopPoint = (stopAtPosition - startPos.x) / 2;
+        }
+        else {
+            stopPoint = (stopAtPosition - startPos.y) / 2;
+        }
+
+        newStopPoint = stopPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!upDownMovement)
-            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
-        else
-            transform.Translate(Vector3.up * Time.deltaTime * movementSpeed);
+        if (!shouldStop){
+            if (!upMovement)
+                transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+            else
+                transform.Translate(Vector3.up * Time.deltaTime * movementSpeed);
+        }
+        else {
+            if (!upMovement)
+                transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+            else
+                transform.Translate(Vector3.up * Time.deltaTime * movementSpeed);
+            slowDown();
+        }
+    }
+
+    void slowDown(){
+        Vector3 currPos = transform.position;
+
+        if (!upMovement){
+            if ((currPos.x - startPos.x) >= newStopPoint) {
+                transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+                movementSpeed = movementSpeed * 0.8f;
+                newStopPoint += stopPoint / slowDownIterator;
+                slowDownIterator *= 2;
+            }
+        }
+        else{
+            if ((currPos.y - startPos.y) >= newStopPoint) {
+                transform.Translate(Vector3.up * Time.deltaTime * movementSpeed);
+                movementSpeed = movementSpeed * 0.8f;
+                newStopPoint += stopPoint / slowDownIterator;
+                slowDownIterator *= 2;
+            }
+        }
+
+        if (currPos.y >= stopAtPosition){
+            movementSpeed = 0;
+        }
     }
 }
