896d63d42015a9586822ac3d942afc31383a9c49
superbode <145613869+superbode@users.noreply.github.com>
2023-12-01 19:54:04 -0500

    Added Tower Cutscene dialogue and music (music decision not final)

--- Assets/Scripts/Cutscenes/TowerDialogueScript.cs
+++ Assets/Scripts/Cutscenes/TowerDialogueScript.cs
@@ -30,16 +30,34 @@ public class TowerCutsceneScript : MonoBehaviour
         // Wait 2 seconds for smoother transition
 		yield return new WaitForSecondsRealtime(7);
 
-		// Elara
-		DialogueController.instance.NewDialogueInstance("Woah. . .","character_elara");
-		
-		yield return new WaitForSecondsRealtime(7);
+        // Elara
+		DialogueController.instance.NewDialogueInstance("Oh. . . we've been here for so long now the sun's coming out.","character_elara");
 
-		// Aiden
-		DialogueController.instance.NewDialogueInstance("You can say that again. . .", "character_aiden");
+        // Aiden
+		DialogueController.instance.NewDialogueInstance("It sure is.", "character_aiden");
+		DialogueController.instance.NewDialogueInstance("And wouldn't you know it. . . there's the tower Mom and Dad worked everyday at.", "character_aiden");
+        DialogueController.instance.NewDialogueInstance("\"The tower that powers Lumina!\" As the world once said.", "character_aiden");
+        DialogueController.instance.NewDialogueInstance("I'm positive THAT is where we'll find information about our parents. After all, the whole city has been feeling like an invitation for us.", "character_aiden");
 
+        // Elara
+		DialogueController.instance.NewDialogueInstance("You think so? I know it's felt like a maze since we got here, but I'm not so sure we're finding what we want to find, Aiden. . .","character_elara");
 
-        yield return new WaitForSecondsRealtime(178);
+        // Aiden
+		DialogueController.instance.NewDialogueInstance("What d'ya mean? It's THE tower our parents worked at!", "character_aiden");
+
+        // Elara
+		DialogueController.instance.NewDialogueInstance("Yes, but. . .","character_elara");
+        DialogueController.instance.NewDialogueInstance("We've been through the sewers and the streets and we've found no clues about our parents. Only that note has been our evidence of something here.","character_elara");
+        DialogueController.instance.NewDialogueInstance("And yet,. . . . . . . there seems to be nothing here.","character_elara");
+
+        // Aiden
+		DialogueController.instance.NewDialogueInstance("Elara, relaaax. I think it's just your tiredness gettin' to ya.", "character_aiden");
+
+        // Elara
+		DialogueController.instance.NewDialogueInstance(". . . . . I guess so.","character_elara");
+        DialogueController.instance.NewDialogueInstance("Well, no time to lose. I'm ready for bed at this point.","character_elara");
+
+        yield return new WaitForSecondsRealtime(91);
 
 		// Fade to next 
 		Initiate.Fade(nextScene, Color.black, transitionSpeed);
